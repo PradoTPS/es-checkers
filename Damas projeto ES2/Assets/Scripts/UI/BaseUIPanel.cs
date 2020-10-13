d76c187af38db593307e7114ba804a92907a52fa
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2020-10-13 17:48:15 -0300

    Game project created and gitignore fixed
    Using version 2019.4.8f1.

---
+++ Damas projeto ES2/Assets/Scripts/UI/BaseUIPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUIPanel : MonoBehaviour {
+    
+    [HideInInspector]
+    public bool isOpen = false;
+
+    public virtual void OpenBehavior()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            gameObject.SetActive(true);
+        }
+    }
+
+    public virtual void UpdateBehavior()
+    {
+        //Empty   
+    }
+
+    public virtual void CloseBehavior()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        isOpen = gameObject.activeSelf;
+    }
+}
---
+++ Damas projeto ES2/Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                var objs = FindObjectsOfType(typeof(T)) as T[];
+
+                if (objs.Length > 0)
+                    _instance = objs[0];
+
+                if (objs.Length > 1)
+                {
+                    Debug.LogWarning("[Singleton] There is more than one instance of " + typeof(T).Name + " in the scene.");
+                    objs[0].DeactivateExcesiveInstances(objs);
+                }
+
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.hideFlags = HideFlags.DontSave;
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+
+        }
+    }
+
+    private void Awake()
+    {
+        var objs = FindObjectsOfType(typeof(T)) as T[];
+        DeactivateExcesiveInstances(objs);
+    }
+
+    public static bool isInicialized
+    {
+        get{return _instance != null;}
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(_instance == this)
+        {
+            _instance = null;
+        }
+    }
+
+    private void DeactivateExcesiveInstances(T[] instancesArray)
+    {
+        for (int i = 1; i < instancesArray.Length; i++)
+            instancesArray[i].gameObject.SetActive(false);
+    }
+}
