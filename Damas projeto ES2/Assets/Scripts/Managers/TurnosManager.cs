e10b2cdaebfb7c239f9ea47268f28d32e430510c
João Pedro Monteiro <joao.monteirodemedeiros@gmail.com>
2020-11-10 19:49:25 -0300

    turno system made

---
+++ Damas projeto ES2/Assets/Scripts/Managers/TurnosManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnosManager : Singleton<TurnosManager>
+{
+
+    public PieceFiliation turnoFiliation;
+
+    public bool CanDoAction(PieceFiliation filiation)
+    {
+        if (filiation == turnoFiliation)
+            return true;
+
+        return false;
+    }
+
+    public void ChangeTurno()
+    {
+        turnoFiliation = turnoFiliation == PieceFiliation.player1 ? PieceFiliation.player2 : PieceFiliation.player1;
+    }
+
+}
